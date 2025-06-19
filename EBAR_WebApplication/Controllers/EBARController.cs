16fa52d27934ce76cf5e7c665d58e88cc1ca4472
Sandy <napiza.sandy.lsei@gmail.com>
2025-06-19 10:23:46 +0800

    Update EBARController.cs

--- EBAR_WebApplication/Controllers/EBARController.cs
+++ EBAR_WebApplication/Controllers/EBARController.cs
@@ -10,11 +10,7 @@ namespace EBAR_WebApplication.Controllers
     {
         EBAR_BL.EBARProcess ebarProcess = new EBAR_BL.EBARProcess();
 
-        /// <summary>
-        /// Retrieves a list of anime titles associated with a specific emoji.
-        /// </summary>
-        /// <param name="emoji" example="😂">The emoji to filter the anime list by.</param>
-        /// <returns>A list of anime titles.</returns>
+   
 
         [HttpGet("View Anime List")]
         public List<string> ViewAnimeList(string emoji)
